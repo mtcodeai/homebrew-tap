0563bf6b28b364a56a7af53e660e29cd65f0a040
zezhenhuang <112902375+zezhenhuang@users.noreply.github.com>
2026-08-03 16:16:46 -0400

    Update mtcode-portal.rb

--- Casks/mtcode-portal.rb
+++ Casks/mtcode-portal.rb
@@ -2,7 +2,6 @@ cask "mtcode-portal" do
   version "1.0.0"
   sha256 "80cf62bebf8659fd0650be12fc333b84a22c93b39210c58ede58803b2b83f54d"
 
-
   url "https://mtcodeai.com/downloads/mtcode-portal/MTCodePortal-macOS-ARM64.dmg"
   name "MTCode Portal"
   desc "Portal and tunnel for MTCode AI GPU servers"
@@ -16,4 +15,12 @@ cask "mtcode-portal" do
   zap trash: [
     "~/Library/Application Support/MTCodePortal",
   ]
+
+  caveats <<~EOS
+    By installing or using MTCode Portal you agree to the license terms:
+      #{appdir}/MTCodePortal.app/Contents/Resources/LICENSE.txt
+    Third-party notices:
+      #{appdir}/MTCodePortal.app/Contents/Resources/third-party-licenses.txt
+  EOS
 end
+
